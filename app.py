43cc5a8f09563addd0382a918280790752cacc79
vaibhavrai17 <vaibhavrai178@gmail.com>
2025-01-24 18:50:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-
-from flask import Flask
+from flask import Flask ,jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Hello, Azure Flask App!'
+
+@app.route('/health', methods=['GET'])
+def health_check():
+    return jsonify({"status": "healthy"}), 200
 
 
 
